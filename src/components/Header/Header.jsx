49b3d6002afc04ed596e369a1c0c79f482bb99c2
Olegopolonets <54370738+Olegopolonets@users.noreply.github.com>
2023-12-28 08:16:34 +0200

    add style for header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import {
   selectIsLoggedIn,
   selectUserName,
 } from '../../store/auth/selectors.js';
 import { logoutThunk } from 'store/auth/operation.js';
 import { HeaderWrapper, NavBar } from './StyledHeader.jsx';
-import { Avatar } from '@mui/material';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  IconButton,
+  Toolbar,
+  Typography,
+} from '@mui/material';
 
 const Header = () => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
@@ -15,28 +23,76 @@ const Header = () => {
   const dispatch = useDispatch();
 
   return (
-    <HeaderWrapper>
-      <h2>Better Call Saul</h2>
+    // <HeaderWrapper>
+    //   <h2>Better Call Saul</h2>
 
-      {user && <h2>Welcome to the club {user}</h2>}
-      <NavBar>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/contacts">Contacts</NavLink>
+    //   {user && <h2>Welcome to the club {user}</h2>}
+    //   <NavBar>
+    //     <NavLink to="/">Home</NavLink>
+    //     <NavLink to="/contacts">Contacts</NavLink>
 
-        {!isLoggedIn && (
-          <>
-            <NavLink to="/register">Sign Up</NavLink>
-            <NavLink to="/login">Login</NavLink>
-          </>
-        )}
+    //     {!isLoggedIn && (
+    //       <>
+    //         <NavLink to="/register">Sign Up</NavLink>
+    //         <NavLink to="/login">Login</NavLink>
+    //       </>
+    //     )}
 
-        {isLoggedIn && (
-          <>
-            | <button onClick={() => dispatch(logoutThunk())}>Exit</button>
-          </>
-        )}
-      </NavBar>
-    </HeaderWrapper>
+    //     {isLoggedIn && (
+    //       <>
+    //         | <button onClick={() => dispatch(logoutThunk())}>Exit</button>
+    //       </>
+    //     )}
+    //   </NavBar>
+    // </HeaderWrapper>
+
+    <Box>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="div"
+            sx={{ flexGrow: 1 }}
+          >
+            <Link to="/">Better Call Saul</Link>
+          </Typography>
+
+          <Typography
+            variant="h6"
+            color="inherit"
+            component="h6"
+            sx={{ flexGrow: 1 }}
+          >
+            {user && <h2>Welcome to the club, {user}!</h2>}
+          </Typography>
+
+          <NavBar sx={{ flexGrow: 1, alignItems: 'center' }}>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/contacts">Contacts</NavLink>
+
+            {!isLoggedIn && (
+              <>
+                <NavLink to="/register">Sign Up</NavLink>
+                <NavLink to="/login">Login</NavLink>
+              </>
+            )}
+
+            {isLoggedIn && (
+              <>
+                <Button
+                  onClick={() => dispatch(logoutThunk())}
+                  variant="contained"
+                  color="error"
+                >
+                  EXIT
+                </Button>
+              </>
+            )}
+          </NavBar>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 };
 
